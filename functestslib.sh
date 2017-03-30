91217e73c2cfe78ad477f1314acd88b5b816ddf8
mhuin <mhuin@redhat.com>
2017-03-30 17:23:42 +0000

    Define a firehose meta-role, add more modularity
    In order to deploy the firehose service, operators only need to specify
    the role "sf-firehose" in their arch file. Furthermore, firehose related
    components will get configured depending on the chosen architecture:
    
    - the mqtt publisher in default jobs will not be created if jenkins is not
      in the arch
    - germqtt will not be set up if gerrit is not in the arch
    - ochlero will not be set up if zuul and nodepool are not in the arch
    
    Also add minimal testing, logging of firehose output.
    
    Change-Id: I7bbfaf287d8090c2be9f7ca815a90b7f3705ef14
    Task: 261

--- functestslib.sh
+++ functestslib.sh
@@ -247,6 +247,21 @@ EOF
     checkpoint "configure_network"
 }
 
+function firehose_listen {
+    [ -e /tmp/firehose.log ] && rm /tmp/firehose.log
+    echo "Listening to Firehose events..."
+    (mosquitto_sub -h sftests.com -t '#' > /tmp/firehose.log & ); FIREHOSE_PID=$!
+}
+
+function firehose_check {
+    kill $FIREHOSE_PID
+    # TODO very basic checks for now
+    echo "Checking Firehose events"
+    grep -q -i "review" /tmp/firehose.log && echo "Review-type events found"
+    grep -q -i "config-update" /tmp/firehose.log && echo "config-update related events found"
+    grep -q -i "PIPELINE" /tmp/firehose.log && echo "Zuul-related events found"
+}
+
 function get_logs {
     #This delay is used to wait a bit before fetching log file from hosts
     #in order to not avoid so important logs that can appears some seconds
@@ -267,6 +282,7 @@ function get_logs {
     [ -d /var/log/Xvfb ] && cp -r /var/log/Xvfb/ ${ARTIFACTS_DIR}/Xvfb
     cp -r /tmp/gui/ ${ARTIFACTS_DIR}/screenshots
 
+    [ -e /tmp/firehose.log ] && cp /tmp/firehose.log ${ARTIFACTS_DIR}/firehose.log
     # Compress gui test
     (ls /tmp/gui/*.avi 1> /dev/null 2>&1) && gzip -9 ${ARTIFACTS_DIR}/screenshots/*.avi
     (ls /tmp/gui/*.mp* 1> /dev/null 2>&1) && gzip -9 ${ARTIFACTS_DIR}/screenshots/*.mp*

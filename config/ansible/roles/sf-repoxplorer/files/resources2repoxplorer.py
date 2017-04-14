e94ed3292339bdc523c54cfb7c5788fb46de8ee1
Fabien Boucher <fabien.dot.boucher@gmail.com>
2017-04-14 18:36:10 +0200

    Add resources2repoxplorer capability
    This patch change the repoxplorer update task to
    run a script that will populate the repoxplorer
    default index file from the resources backend content.
    
    Only groups and projects are populated. Repositories
    not attached to a projects are skipped.
    
    Story: 76
    Task: 274
    
    Change-Id: Ice1bbaa75987bbf2e0a3fbb96ece340327e427e0

---
+++ config/ansible/roles/sf-repoxplorer/files/resources2repoxplorer.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+
+import os
+import sys
+import yaml
+import shutil
+import requests
+
+REPOXPLORER_DEFAULT_FILE = "/etc/repoxplorer/default.yaml"
+
+if __name__ == "__main__":
+    default = yaml.safe_load(file(REPOXPLORER_DEFAULT_FILE).read())
+    resources = requests.get("http://managesf:20001/resources/").json()
+    resources = resources['resources']
+    for project, data in resources['projects'].items():
+        default['projects'][project] = {}
+        for repo in data['source-repositories']:
+            default['projects'][project][repo] = {'template': 'default'}
+    for group, data in resources['groups'].items():
+        grp = {}
+        grp['description'] = data['description']
+        grp['emails'] = dict((member, None) for member in data['members'])
+        default['groups'][group] = grp
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'apply':
+        shutil.copy(REPOXPLORER_DEFAULT_FILE,
+                    REPOXPLORER_DEFAULT_FILE + 'save')
+        yaml.safe_dump(default,
+                       file(REPOXPLORER_DEFAULT_FILE, 'w'),
+                       default_flow_style=False)
+    else:
+        print default
+        print
+        print "Run with 'apply' as first argument to apply"

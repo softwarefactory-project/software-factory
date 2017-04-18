4abe944037b6a8a675cb12d0a4b35d76be2a0176
Tristan Cacqueray <tdecacqu@redhat.com>
2017-04-18 11:41:27 +0000

    Remove epel dependency
    Change-Id: If866558207d3d3d63c58986b1b6c631a2675eda6

--- rpm-requirements.sh
+++ rpm-requirements.sh
@@ -3,24 +3,16 @@
 
 PKGS=""
 which wget &> /dev/null         || PKGS="${PKGS} wget"
-which gcc &> /dev/null          || PKGS="${PKGS} gcc"
 which git &> /dev/null          || PKGS="${PKGS} git"
 which curl &> /dev/null         || PKGS="${PKGS} curl"
 which pigz &> /dev/null         || PKGS="${PKGS} pigz"
 which patch &> /dev/null        || PKGS="${PKGS} patch"
-which virtualenv &> /dev/null   || PKGS="${PKGS} python-virtualenv"
-which pip &> /dev/null          || PKGS="${PKGS} python-pip"
 [ -f "/usr/include/ffi.h" ]     || PKGS="${PKGS} libffi-devel mariadb-devel openldap-devel openssl-devel"
 [ -f "/usr/include/python2.7/Python.h" ] || PKGS="${PKGS} python-devel"
 [ -d "/usr/lib64/python2.7/site-packages/Crypto/" ] || PKGS="${PKGS} python2-crypto"
 [ -d "/usr/lib/python2.7/site-packages/jinja2/" ] || PKGS="${PKGS} python-jinja2"
 [ -f "/usr/lib64/libvirt/connection-driver/libvirt_driver_lxc.so" ] || PKGS="${PKGS} libvirt-daemon-driver-lxc libvirt-daemon-lxc libvirt"
 [ -d "/usr/lib/python2.7/site-packages/mock" ] || PKGS="${PKGS} python2-mock"
-
-if [ ! -f "/etc/yum.repos.d/epel.repo" ]; then
-    echo "(+) Adds epel-release..."
-    sudo yum install -y epel-release
-fi
 
 if [ ! -z "${PKGS}" ]; then
     echo "(+) Installing build requirements..."

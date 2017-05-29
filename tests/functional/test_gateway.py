b832013e865953a93097a1581157ee91112fb86d
Fabien Boucher <fboucher@redhat.com>
2017-05-29 14:19:30 +0200

    Remove exec bit on most of our tests - they were bypassed ...
    And two tests fixes already added in sf-ci.
    
    Change-Id: Ic5131b28d456396961f6d87e69b220180c51af3a

--- tests/functional/test_gateway.py
+++ tests/functional/test_gateway.py
@@ -150,7 +150,7 @@ class TestGateway(Base):
 
         # Ensure jenkins cli is disabled
         resp = requests.get(url + "cli/")
-        self.assertEqual(resp.status_code, 503)
+        self.assertEqual(resp.status_code, 403)
 
         # With SSO cookie
         resp = requests.get(
@@ -159,15 +159,23 @@ class TestGateway(Base):
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('<title>Dashboard [Jenkins]</title>' in resp.text)
 
-        # User shouldn't be known in Jenkins if logged in with SSO
-        self.assertTrue(config.USER_1 not in resp.text)
+        # User1 (admin ) be known in Jenkins if logged in with SSO
+        self.assertTrue(config.USER_1 in resp.text)
+
+        # With SSO cookie as normal unpriviledge user
+        resp = requests.get(
+            url, cookies=dict(
+                auth_pubtkt=config.USERS[config.USER_2]['auth_cookie']))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('<title>Dashboard [Jenkins]</title>' in resp.text)
+        self.assertTrue(config.USER_2 not in resp.text)
 
         # Ensure jenkins cli is disabled for SSO user
         resp = requests.get(
             url + "cli/",
             cookies=dict(
                 auth_pubtkt=config.USERS[config.USER_1]['auth_cookie']))
-        self.assertEqual(resp.status_code, 503)
+        self.assertEqual(resp.status_code, 403)
 
     def test_kibana_accessible(self):
         """ Test if Kibana is accessible on gateway host
--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -462,7 +462,7 @@ class JenkinsUtils:
 
     def wait_for_config_update(self, revision):
         job_text = "Updating configuration using %s" % revision
-        for retry in xrange(60):
+        for retry in xrange(120):
             time.sleep(1)
             job_log = self.get_last_console("config-update")
             if job_text in job_log and "Finished: " in job_log:
